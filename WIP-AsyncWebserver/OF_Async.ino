15917b4c87707a5737db74173b43c062869e984a
Disaster of Puppets <62915210+DisasterofPuppets@users.noreply.github.com>
2025-08-25 17:32:44 +1000

    Update OF_Async.ino
    that servo thing, yeah fixed

--- WIP-AsyncWebserver/OF_Async.ino
+++ WIP-AsyncWebserver/OF_Async.ino
@@ -92,7 +92,7 @@ void setup() {
   logBuffer = ""; 
 
   Serial.begin(115200);
-
+  currentPulse = angleToMicros(CLOSED_ANGLE); // Initialize to closed position
 //reads the system memory states
   system_rtc_mem_read(RTC_MEM_START, &rtcData, sizeof(rtcData));
   
